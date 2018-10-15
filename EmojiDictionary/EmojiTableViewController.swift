43dc1bfa415a7d3f4c678ff7173e976c3b12384d
Reid, Jon Carl <jreid2@hk220-mac1.local>
2018-10-15 15:28:36 -0700

    Initial Push

---
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiDictionary
+//
+//  Created by Reid, Jon Carl on 10/15/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    var emojis: [Emoji] = [
+        Emoji("😀", "Grinning Face", "A typical smiley face.", "happiness"),
+        Emoji("😕", "Confused Face", "A confused, puzzled face.", "unsure what to think, displeasure"),
+        Emoji("😍", "Heart Eyes", "A smiley face with hearts for eyes.", "love of something; attractive"),
+        Emoji("👮", "Police Officer", "A police officer wearing a blue cap with a gold badge", "person of authority"),
+        Emoji("🐢", "Turtle", "A cute turtle.", "Something slow"),
+        Emoji("🐘", "Elephant", "A gray elephant.", "good memory"),
+        Emoji("🍝", "Spaghetti", "A plate of spaghetti.", "spaghetti"),
+        Emoji("🎲", "Die", "A single die.", "taking a risk, chance; game"),
+        Emoji("⛺️", "Tent", "A small tent.", "camping"),
+        Emoji("📚", "Stack of Books", "Three colored books stacked on each other.", "homework, studying"),
+        Emoji("💔", "Broken Heart", "A red, broken heart.", "extreme sadness"),
+        Emoji("💤", "Snore", "Three blue \'z\'s.", "tired, sleepiness"),
+        Emoji("🏁", "Checkered Flag", "A black-and-white checkered flag.", "completion")]
+    
+}
